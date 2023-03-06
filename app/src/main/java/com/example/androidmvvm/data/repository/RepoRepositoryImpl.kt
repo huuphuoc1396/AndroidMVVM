3d4b3561b7e8b3e55250413b4f2b79498c71665e
huuphuoc1396 <huuphuoc1396@gmail.com>
2023-03-06 15:44:12 +0700

    Missing flow on IO for request API

--- app/src/main/java/com/example/androidmvvm/data/repository/RepoRepositoryImpl.kt
+++ app/src/main/java/com/example/androidmvvm/data/repository/RepoRepositoryImpl.kt
@@ -4,7 +4,9 @@ import com.example.androidmvvm.data.remote.response.search_repo.toRepoItem
 import com.example.androidmvvm.data.remote.retrofit.api.ApiService
 import com.example.androidmvvm.domain.model.repo.RepoModel
 import com.example.androidmvvm.domain.repository.RepoRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
 class RepoRepositoryImpl constructor(
@@ -14,6 +16,6 @@ class RepoRepositoryImpl constructor(
     override suspend fun searchRepos(query: String): Flow<List<RepoModel>> {
         return apiService.searchRepos(query).map { searchRepoResponse ->
             searchRepoResponse.items?.map { item -> item.toRepoItem() }.orEmpty()
-        }
+        }.flowOn(Dispatchers.IO)
     }
 }
